9ee34dc466eb5076f50b5e7cc692612e10fe6d97
Naphtali Morage <naphtalimorage56@example.com>
2024-10-03 13:08:24 +0300

    impliment a fetch lyrics function and two inputs for artist name and song name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
-import './index.css';
+import React from "react";
+import "./index.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [artists, setArtists] = useState("");
+  const [songs, setSongs] = useState("");
+  const [lyrics, setLyrics] = useState("");
+
+  useEffect(() => {
+    const fetchLyrics = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.lyrics.ovh/v1/${artists}/${songs}`
+        );
+        if (!response.ok) {
+          throw new Error(`API request failed with status ${response.status}`);
+        }
+        const data = await response.json();
+        setLyrics(data.lyrics);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchLyrics();
+  }, [artists, songs]);
+
   return (
-    <div className="">
-      
-    </div>
+  <div className="flex flex-col justify-center content-center">
+      <h1 className="text-xl font-serif font-bold">Find Lyrics</h1>
+      <div>
+      <input type="text" placeholder="Artist Name" onChange={(e) => {setArtists(e.target.value)}}/>
+      <input type="text" placeholder="Song Title" onChange={(e) => {setSongs(e.target.value)}}/>
+      </div>
+      <p>{lyrics}</p>
+  </div>
   );
+    
+  
 }
 
 export default App;
